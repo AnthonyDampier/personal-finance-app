ef61af3fcaf6673e56e9a3a1a5bab9ae3792f1cc
AnthonyDampier <Dampieranthony2@gmail.com>
2024-02-21 17:57:27 -0600

    Add HomeCalculations component

---
+++ src/components/HomeCalculations/HomeCalculations.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const HomeCalculations = ({userInfo, userFinancialInfo, userMortgageInfo}) => {
+
+    return (
+        <div>
+            <h1>Home Calculations</h1>
+            <p>Calculate the cost of your home</p>
+        </div>
+    );
+}
+
+export default HomeCalculations;
