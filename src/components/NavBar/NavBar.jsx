28b52e4f772dc2673afdefe80f1f0c5d958258e6
AnthonyDampier <Dampieranthony2@gmail.com>
2024-02-22 08:50:16 -0600

    Add NavBar component with CSS styling

---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './NavBar.css';
+
+const NavBar = () => {
+    return (
+    <header>
+        <nav class="apple-nav">
+            <ul class="nav-links">
+                <li><Link to="/">Home</Link></li>
+                <li><Link to="/MortgageCalcs">Mortgage</Link></li>
+                <li><Link to="/VehicleCalcs">Vehicle</Link></li>
+                <li><Link to="/retirement">Retirement</Link></li>
+                <li><Link to="/contact">Contact</Link></li>
+            </ul>
+        </nav>
+    </header>
+    )
+};
+
+export default NavBar;
