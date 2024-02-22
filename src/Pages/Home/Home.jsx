0ef608ec253d078bd5efc99e0bade5cc7e6ba62d
AnthonyDampier <Dampieranthony2@gmail.com>
2024-02-22 09:53:36 -0600

    Refactor Home page components and styles

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,41 +1,27 @@
 import React from 'react';
 import './Home.css';
-import { Link } from 'react-router-dom';
 
-const Home = () => {
-    return (
-        <>
-            <main class="main-content">
-                <section class="hero-section">
-                    <h1>Welcome to your Personal Finance Application</h1>
-                    <p>Discover our news and financial calculator that fit your needs</p>
-                    {/* <button class="explore-button">Explore Now</button> */}
-                </section>
+// Assuming you have components for each part
+import HeroSection from '../../components/HomeComponents/HeroSection/HeroSection';
+import FeaturedContent from '../../components/HomeComponents/FeaturedContent/FeaturedContent';
+import CallToAction from '../../components/HomeComponents/CallToAction/CallToAction';
+// import SidebarAds from '../../components/SidebarAds/SidebarAds';
+import ContactForm from '../../components/HomeComponents/ContactForm/ContactForm';
 
-                <section class="features-section">
-                    <h2>Our Features</h2>
-                    <div class="features-grid">
-                        <Link to="/MortgageCalcs">
-                            <div class="feature-item">
-                                <h3>Mortgage Calculators</h3>
-                                <p>Calculate the health of your mortgage and find information to help you understand your mortgage, the mortgage market, and discover opportunities to save!</p>
-                            </div>
-                        </Link>
-                        <Link to="/VehicleCalcs">
-                            <div class="feature-item">
-                                <h3>Vehicle Calculators</h3>
-                                <p>Description of Feature 2.</p>
-                            </div>
-                        </Link>
-                        <div class="feature-item">
-                            <h3>Feature 3</h3>
-                            <p>Description of Feature 3.</p>
-                        </div>
-                    </div>
-                </section>
-            </main>
-        </>
-    );
-}
 
-export default Home;
+const HomePage = () => {
+  return (
+    <main className="homepage main-content">
+      <HeroSection />
+      <CallToAction />
+    <FeaturedContent />
+    {/* <BlogReferences /> */}
+    <ContactForm />
+    <div className="sidebar">
+        {/* <SidebarAds /> */}
+    </div>
+    </main>
+  );
+};
+
+export default HomePage;
