55738e3af4e09fef76931214146cb15e4a036bff
AnthonyDampier <Dampieranthony2@gmail.com>
2024-02-22 09:53:24 -0600

    Add HeroSection component and its styling

---
+++ src/components/HomeComponents/HeroSection/HeroSection.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import './HeroSection.css';
+
+const HeroSection = () => { 
+    return (
+        <section class="hero-section">
+            <h1>Welcome to your Personal Finance Application</h1>
+            <p>Discover our news and financial calculator that fit your needs</p>
+            {/* <button class="explore-button">Explore Now</button> */}
+        </section>
+    );
+}
+
+export default HeroSection;
