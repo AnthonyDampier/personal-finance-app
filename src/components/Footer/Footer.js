4344adcbe1808c448b16949190f194c7b3eb3ec7
AnthonyDampier <Dampieranthony2@gmail.com>
2024-02-22 11:10:01 -0600

    Add social media links to footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -6,6 +6,14 @@ const Footer = () => {
     return (
         <footer className="apple-footer">
             <p>© {year} Anthony Dampier</p>
+            <a href="https://github.com/anthonydampier" target="_blank" rel="noopener noreferrer">
+                {/* Replace src with the path to your GitHub logo */}
+                <img src="https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png" alt="GitHub" style={{width: '24px', height: '24px'}} />
+            </a>
+            <a href="https://linkedin.com/in/anthonydampier" target="_blank" rel="noopener noreferrer">
+                {/* Replace src with the path to your LinkedIn logo */}
+                <img src="https://upload.wikimedia.org/wikipedia/commons/e/e9/Linkedin_icon.svg" alt="LinkedIn" style={{width: '24px', height: '24px'}} />
+            </a>
         </footer>
     )
 };
