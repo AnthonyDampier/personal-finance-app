f00b591142731b0d13280f24ea6456a7d2117e69
AnthonyDampier <Dampieranthony2@gmail.com>
2024-02-22 11:41:29 -0600

    Update CallToAction and HeroSection components

--- src/components/HomeComponents/CallToAction/CallToAction.jsx
+++ src/components/HomeComponents/CallToAction/CallToAction.jsx
@@ -6,7 +6,7 @@ const CallToAction = () => {
         <div className="cta-container">
             <h2>Discover Your Economic Health</h2>
             <p>Use our easy tools to improve your financial literacy today.</p>
-            <button className="cta-button" onClick={() => window.location.href='/calculators'}>Start Now</button>
+            <button className="cta-button" onClick={() => window.location.href='/MortgageCalcs'}>Start Now</button>
         </div>
     );
 };
--- src/components/HomeComponents/HeroSection/HeroSection.jsx
+++ src/components/HomeComponents/HeroSection/HeroSection.jsx
@@ -6,7 +6,6 @@ const HeroSection = () => {
         <section className="hero-section">
             <h1>Welcome to your Personal Finance Application</h1>
             <p>Discover our news and financial calculator that fit your needs</p>
-            {/* <button className="explore-button">Explore Now</button> */}
         </section>
     );
 }
